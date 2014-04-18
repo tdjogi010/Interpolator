108567d9e98162df2753d5eb954127bfdcdfaaf0
MUDIT DANGI <itboymd@itboymd-SVF15218SNB.(none)>
2014-04-18 17:31:29 +0530

    layout new

--- Interpolator/src/main/java/com/mac/interpolator/interpolator/Main_interpolator.java
+++ Interpolator/src/main/java/com/mac/interpolator/interpolator/Main_interpolator.java
@@ -5,18 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.view.View.OnClickListener;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Main_interpolator extends Activity implements OnClickListener {
 
     EditText y2,y1,x2,x1,x;
     TextView y;
-    Button go;
-    RelativeLayout rl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +24,22 @@ public class Main_interpolator extends Activity implements OnClickListener {
         x2 = (EditText) findViewById(R.id.et_x2);
         x1 = (EditText) findViewById(R.id.et_x1);
         x = (EditText) findViewById(R.id.et_x);
-        y = (TextView) findViewById(R.id.et_y);
-        go= (Button) findViewById(R.id.b_go);
-        rl = (RelativeLayout) findViewById(R.id.lay_main);
-        rl.setHorizontalScrollBarEnabled(true);
+        y = (TextView) findViewById(R.id.tv_y);
 
-        y2.setText("0");
-        y1.setText("0");
-        x2.setText("0");
-        x1.setText("0");
-        x.setText("0");
-
-        go.setOnClickListener(this);
 
         y2.setOnClickListener(this);
-       /* y1.setOnClickListener(this);
+        y1.setOnClickListener(this);
         x2.setOnClickListener(this);
         x1.setOnClickListener(this);
-        x.setOnClickListener(this);*/
+        x.setOnClickListener(this);
 
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-    // Inflate the menu; this adds items to the action bar if it is present.
+
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_interpolator, menu);
         return true;
     }
@@ -72,15 +58,6 @@ public class Main_interpolator extends Activity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-
-        float vy2= Float.parseFloat(y2.getText().toString());
-        float vy1= Float.parseFloat(y1.getText().toString());
-        float vx2= Float.parseFloat(x2.getText().toString());
-        float vx1= Float.parseFloat(x1.getText().toString());
-        float vx= Float.parseFloat(x.getText().toString());
-
-        float vy=(vy2-vy1)*(vx-vx1)/(vx2-vx1) + vy1;
-
-        y.setText(""+vy);
+        //y.setText((y2.getText()-y1.getText())*(x.getText()-x1.getText())/(x2.getText()-x1.getText()));
     }
 }
