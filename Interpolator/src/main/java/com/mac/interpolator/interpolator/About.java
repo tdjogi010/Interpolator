b44264c2469362ce622d6c396cbe692f72fa68d4
Mudit18 <mudit.d7869@gmail.com>
2015-02-10 14:05:16 +0530

    New Design : Material Theme, Toolbar.

--- Interpolator/src/main/java/com/mac/interpolator/interpolator/About.java
+++ Interpolator/src/main/java/com/mac/interpolator/interpolator/About.java
@@ -1,17 +1,42 @@
 package com.mac.interpolator.interpolator;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 /**
  * Created by itboymd on 19/4/14.
  */
-public class About extends Activity{
+public class About extends ActionBarActivity {
+
+    TextView abt,par1,par2,cred,md,tj;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.about);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.my_toolbar_about);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        /*abt = (TextView) findViewById(R.id.about);
+        abt.setTypeface(Main_interpolator.font, Typeface.BOLD);*/
+        cred = (TextView) findViewById(R.id.credits);
+        cred.setTypeface(Main_interpolator.font,Typeface.BOLD);
+        par1 = (TextView) findViewById(R.id.para1);
+        par1.setTypeface(Main_interpolator.font,Typeface.NORMAL);
+        par2 = (TextView) findViewById(R.id.para2);
+        par2.setTypeface(Main_interpolator.font,Typeface.NORMAL);
+        md = (TextView) findViewById(R.id.MD);
+        md.setTypeface(Main_interpolator.font,Typeface.BOLD);
+        tj = (TextView) findViewById(R.id.TJ);
+        tj.setTypeface(Main_interpolator.font,Typeface.BOLD);
     }
 
 }
--- Interpolator/src/main/java/com/mac/interpolator/interpolator/Main_interpolator.java
+++ Interpolator/src/main/java/com/mac/interpolator/interpolator/Main_interpolator.java
@@ -1,51 +1,77 @@
 package com.mac.interpolator.interpolator;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Main_interpolator extends Activity implements TextWatcher {
+public class Main_interpolator extends ActionBarActivity implements TextWatcher {
 
-    EditText y2,y1,x2,x1,x;
-    TextView y;
+    EditText et_y2, et_y1, et_x2, et_x1, et_x;
+    TextView y,x1,x2,y1,y2,x,result,dev,mac,bitsg;
+    static Typeface fontthin,font,fontbold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        ActionBar actionbar=getActionBar();
-        actionbar.show();
+        //ActionBar actionbar=getActionBar();
+        //actionbar.show();
         setContentView(R.layout.layout_main_interpolator);
-        y2 = (EditText) findViewById(R.id.et_y2);
-        y1 = (EditText) findViewById(R.id.et_y1);
-        x2 = (EditText) findViewById(R.id.et_x2);
-        x1 = (EditText) findViewById(R.id.et_x1);
-        x = (EditText) findViewById(R.id.et_x);
-        y = (TextView) findViewById(R.id.tv_y);
 
-        y2.setHint("0");
-        y1.setHint("0");
-        x2.setHint("0");
-        x1.setHint("0");
-        x.setHint("0");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
 
+        font= Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+        fontthin= Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
+        fontbold=Typeface.createFromAsset(getAssets(), "fonts/Roboto-Bold.ttf");
 
-        y2.addTextChangedListener(this) ;
-        y1.addTextChangedListener(this) ;
-        x2.addTextChangedListener(this) ;
-        x1.addTextChangedListener(this) ;
-        x.addTextChangedListener(this) ;
+        et_y2 = (EditText) findViewById(R.id.et_y2);
+        et_y2.setTypeface(font,Typeface.NORMAL);
+        et_y1 = (EditText) findViewById(R.id.et_y1);
+        et_y1.setTypeface(font,Typeface.NORMAL);
+        et_x2 = (EditText) findViewById(R.id.et_x2);
+        et_x2.setTypeface(font,Typeface.NORMAL);
+        et_x1 = (EditText) findViewById(R.id.et_x1);
+        et_x1.setTypeface(font, Typeface.NORMAL);
+        et_x = (EditText) findViewById(R.id.et_x);
+        et_x.setTypeface(font, Typeface.NORMAL);
+        y = (TextView) findViewById(R.id.tv_res);
+        y.setTypeface(font, Typeface.NORMAL);
+        x = (TextView) findViewById(R.id.tv_x);
+        x.setTypeface(font,Typeface.NORMAL);
+        x1 = (TextView) findViewById(R.id.tv_x1);
+        x1.setTypeface(font,Typeface.NORMAL);
+        x2 = (TextView) findViewById(R.id.tv_x2);
+        x2.setTypeface(font,Typeface.NORMAL);
+        y1 = (TextView) findViewById(R.id.tv_y1);
+        y1.setTypeface(font,Typeface.NORMAL);
+        y2 = (TextView) findViewById(R.id.tv_y2);
+        y2.setTypeface(font,Typeface.NORMAL);
+        y2 = (TextView) findViewById(R.id.tv_y2);
+        y2.setTypeface(font,Typeface.NORMAL);
+        result = (TextView) findViewById(R.id.tv_res);
+        result.setTypeface(font,Typeface.NORMAL);
+        dev = (TextView) findViewById(R.id.dev);
+        dev.setTypeface(font,Typeface.NORMAL);
+        mac = (TextView) findViewById(R.id.MAC);
+        mac.setTypeface(font,Typeface.BOLD);
+        bitsg = (TextView) findViewById(R.id.bitsg);
+        bitsg.setTypeface(font,Typeface.NORMAL);
+
+        et_y2.addTextChangedListener(this) ;
+        et_y1.addTextChangedListener(this) ;
+        et_x2.addTextChangedListener(this) ;
+        et_x1.addTextChangedListener(this) ;
+        et_x.addTextChangedListener(this) ;
 
     }
 
@@ -89,11 +115,11 @@ public class Main_interpolator extends Activity implements TextWatcher {
     @Override
     public void afterTextChanged(Editable s) {
         try {
-            float vy2 = Float.parseFloat(y2.getText().toString());
-            float vy1 = Float.parseFloat(y1.getText().toString());
-            float vx2 = Float.parseFloat(x2.getText().toString());
-            float vx1 = Float.parseFloat(x1.getText().toString());
-            float vx = Float.parseFloat(x.getText().toString());
+            float vy2 = Float.parseFloat(et_y2.getText().toString());
+            float vy1 = Float.parseFloat(et_y1.getText().toString());
+            float vx2 = Float.parseFloat(et_x2.getText().toString());
+            float vx1 = Float.parseFloat(et_x1.getText().toString());
+            float vx = Float.parseFloat(et_x.getText().toString());
             float vy = (vy2 - vy1) * (vx - vx1) / (vx2 - vx1) + vy1;
             y.setText("" + vy);
         } catch (NumberFormatException e) {
